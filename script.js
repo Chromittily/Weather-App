731776ca90e7311e6c27ab3ae272631f84e80065
Chromittily <chromittily@gmail.com>
2022-07-30 18:31:25 -0500

    Finished connecting JSON data to HTML classes

---
+++ script.js
@@ -0,0 +1,27 @@
+let weather = {
+    apiKey: "069c4c1ff33df9131e97d6e4af9cc7cf",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data)); //Changed from console.log() >> displayWeather()
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0]; //Data is originally contained in an array, so "[0]" is needed.
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    }
+};
