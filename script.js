e59576da117f8d3d1bffd1ea233a7980d8971d27
Tiana <chromittily@gmail.com>
2025-05-04 22:33:14 -0500

    Utilizing Unsplash API
    - Updated CSS to have Unsplash API url call for the background; INCOMPLETE, this returns JSON info only.
    - Updated index.html to include attribution info of photographer when background is properly implemented.
    - Updated script.js with Unsplash API Tutorial code to learn how to make calls to the API; must credit 2nd tutorial in ReadMe file.

--- script.js
+++ script.js
@@ -19,12 +19,13 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src =
             "http://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = parseInt((temp* 9/5) + 32) + "°F";
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')";
+        document.body.style.backgroundImage = "url('https://api.unsplash.com/photos/random/?query=" + name + "&client_id=znH73UCnuEIr20_sM44KszkeLkgl9Zv2w8xGu70Ulkw')";
+        //document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')";
        // document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1659200936483-65f3fd694065?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
     },
 
@@ -48,3 +49,19 @@ document
 });
 
 weather.fetchWeather("Birmingham");
+
+
+
+
+let clientID = "znH73UCnuEIr20_sM44KszkeLkgl9Zv2w8xGu70Ulkw";
+//let endpoint = `https://api.unsplash.com/search/photos?page=1&query=office/?client_id=${clientID}`;
+let endpoint = `https://api.unsplash.com/photos/random/?query=office&client_id=${clientID}`;
+
+fetch(endpoint)
+    .then(function (response){
+       // console.log(response.json());
+       return response.json();
+    })
+    .then(function (jsonData){
+        console.log(jsonData);
+    })
