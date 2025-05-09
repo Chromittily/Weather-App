60c8cbdfde23f559ea631de207712a7c77ad9be7
Tiana <chromittily@gmail.com>
2025-05-08 23:56:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let weather = {
     apiKey: "069c4c1ff33df9131e97d6e4af9cc7cf",
-    fetchWeather: function(city) {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" 
-            + city 
-            + "&units=metric&appid=" 
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
             + this.apiKey
         )
             .then((response) => response.json())
             .then((data) => this.displayWeather(data)); //Changed from console.log() >> displayWeather()
     },
-    displayWeather: function(data) {
+    displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0]; //Data is originally contained in an array, so "[0]" is needed.
         const { temp, humidity } = data.main;
@@ -19,14 +19,14 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src =
             "http://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".temp").innerText = parseInt((temp* 9/5) + 32) + "°F";
+        document.querySelector(".temp").innerText = parseInt((temp * 9 / 5) + 32) + "°F";
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
         //document.body.style.backgroundImage = "url('https://api.unsplash.com/photos/random/?query=" + name + "&client_id=znH73UCnuEIr20_sM44KszkeLkgl9Zv2w8xGu70Ulkw')";
         //document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')";
-       // document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1659200936483-65f3fd694065?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
+        // document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1659200936483-65f3fd694065?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
     },
 
     search: function () {
@@ -36,17 +36,19 @@ let weather = {
 
 document
     .querySelector(".search button")
-    .addEventListener("click", function() {
+    .addEventListener("click", function () {
         weather.search();
-});
+        websiteBackground.photoSearch();
+    });
 
 document
     .querySelector(".search-bar")
-    .addEventListener("keyup", function (event){
-        if(event.key == "Enter") {
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
             weather.search();
+            websiteBackground.photoSearch();
         }
-});
+    });
 
 
 
@@ -60,7 +62,7 @@ let endpoint = `https://api.unsplash.com/photos/random/?query=Birmingham&client_
 let imageElement = document.querySelector("#unsplashImage");
 let imageLink = document.querySelector("#imageLink");
 let creator = document.querySelector("#creator");
-
+/*
 fetch(endpoint)
     .then(function (response){
        // console.log(response.json());
@@ -71,5 +73,41 @@ fetch(endpoint)
         document.body.style.backgroundImage = `url('${jsonData.urls.regular}')`;
         //imageElement.src = jsonData.urls.regular;
     });
+*/
 
-    weather.fetchWeather("Birmingham");
+/** 
+ * Below, I am attempting to make the above code 
+ * look like the intial 'weather' function.
+ * I want to call this function when the weather is updated.
+*/
+
+let websiteBackground = {
+
+    unsplashApiKey: "znH73UCnuEIr20_sM44KszkeLkgl9Zv2w8xGu70Ulkw",
+    fetchBackground: function (city) {
+        fetch(
+            "https://api.unsplash.com/photos/random/?query="
+            + city
+            + "&client_id="
+            + this.unsplashApiKey
+        )
+            .then((response) => response.json())
+            .then((jsonData) => this.fetchBackground(jsonData));
+
+    },
+
+    setBackground: function (jsonData) {
+        console.log(jsonData);
+        const { regularURL } = jsonData.urls.regular;
+        document.body.style.backgroundImage = url(regularURL);
+    },
+
+    photoSearch: function () {
+        this.fetchBackground(document.querySelector(".search-bar").value)
+    }
+
+};
+
+
+weather.fetchWeather("Birmingham");
+websiteBackground.fetchBackground("Birmingham");
